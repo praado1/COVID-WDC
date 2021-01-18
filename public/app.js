3450db5dfcab36f2303a70170e6f989b7a5b80d3
praado1 <diegoprado@ufm.edu>
2021-01-18 17:23:10 -0600

    MESSAGE

--- public/app.js
+++ public/app.js
@@ -1,1 +1,16 @@
 console.log("This is Working!")
+
+
+(function () {
+    var myConnector = tableau.makeConnector();
+
+    myConnector.getSchema = function (schemaCallback) {
+
+    };
+
+    myConnector.getData = function (table, doneCallback) {
+
+    };
+
+    tableau.registerConnector(myConnector);
+})();
