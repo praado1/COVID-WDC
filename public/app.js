6ba7021cdd0d0909d94cf2c3a80a4f8b23ae1f35
praado1 <diegoprado@ufm.edu>
2021-01-18 17:41:05 -0600

    MESSAGE

--- public/app.js
+++ public/app.js
@@ -4,7 +4,47 @@ console.log("This is Working!");
 (function () {
     var myConnector = tableau.makeConnector();
 
-    myConnector.getSchema = function (schemaCallback) {
+    myConnector.getSchema = function (schemaCallback) { 
+        const covidCols = [
+        {
+          id: "Date_of_report",
+          dataType: tableau.dataTypeEnum.date,
+        },
+        {
+          id: "Municipality_code",
+          dataType: tableau.dataTypeEnum.string,
+        },
+        {
+          id: "Municipality_name",
+          dataType: tableau.dataTypeEnum.string,
+        },
+        {
+          id: "Province",
+          dataType: tableau.dataTypeEnum.string,
+        },
+        {
+          id: "Total_reported",
+          dataType: tableau.dataTypeEnum.int,
+        },
+        {
+          id: "Hospital_admission",
+          dataType: tableau.dataTypeEnum.int,
+        },
+        {
+          id: "Deceased",
+          dataType: tableau.dataTypeEnum.int,
+        },
+      ];
+  
+      let covidTableSchema = {
+        id: "RIVM",
+        alias: "Dutch Corona Cases since start",
+        columns: covidCols,
+      };
+
+  
+      schemaCallback([covidTableSchema]);
+    
 
     };
 
